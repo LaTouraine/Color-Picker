200f4685708a736260a7971ec388d57f04f317e1
Michael Cortella <mcortella@cnm.edu>
2025-05-06 17:00:51 -0600

    check 2

--- script.js
+++ script.js
@@ -8,19 +8,34 @@ function initColorPicker() {
         red: document.getElementById("red"),
         green: document.getElementById("green"),
         blue: document.getElementById("blue")
-
     };
-    setColorPickerEventListeners(colorBox, rgb);
+    let colorPickers = document.getElementsByClassName("picker");
+    setColorPickerEventListeners(colorBox, rgb, colorPickers);
 }
 
-function setColorPickerEventListeners(colorBox, rgb) {
-    rgb.red.addEventListener('change', () => {
-        console.log("Red value: ", rgb.red.value);
-    });
-    rgb.green.addEventListener('change', () => {
-        console.log("Green value: ", rgb.green.value);
-    });
-    rgb.blue.addEventListener('change', () => {
-        console.log("Blue value: ", rgb.blue.value);
-    });
+function setColorPickerEventListeners(elements, colors, pickerElements) {
+    for (let i = 0; i < pickerElements.length; i++) {
+        pickerElements[i].addEventListener('change', () => {
+        let red = colors.red.value;
+        let green = colors.green.value;
+        let blue = colors.blue.value;
+        setElementBGColor(element, red, green, blue);
+        setDisplayValues(red, green, blue);
+        });
+    }
+}
+
+function setColor(colorBox, red, green, blue) {
+    let rgbVal = [red, green, blue].join(',');
+    BGElement.style.backgroundColor = "rgb(" + rgbVal + ")";
+}
+
+function setDisplayValues(red, green, blue) {
+    let redVal = document.getElementById("red-val");
+    let greenVal = document.getElementById("green-val");
+    let blueVal = document.getElementById("blue-val");
+
+    redVal.innerText = red;
+    greenVal.innerText = green;
+    blueVal.innerText = blue;
 }
